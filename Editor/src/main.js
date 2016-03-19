23339dad1111548075cc9f5ac0a87bb3a2278d30
Cedric Stoquer <cstoquer@wizcorp.jp>
2016-03-19 11:47:47 +0900

    pixelbox editor

---
+++ Editor/src/main.js
@@ -0,0 +1,11 @@
+var test = getMap('test');
+
+draw(test);
+
+return;
+
+//▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+// Update is called once per frame
+exports.update = function () {
+	
+};
